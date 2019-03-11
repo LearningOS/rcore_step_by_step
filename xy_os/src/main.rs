3142466233d0fa8b58f2aa41a02a7102b135673f
刘丰源 <2017011313@secoder.net>
2019-03-11 21:42:12 +0800

    有了暂时可用的trap

--- xy_os/src/main.rs
+++ xy_os/src/main.rs
@@ -1,13 +1,54 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(global_asm)]
+#![feature(asm)]
 
 #[macro_use]
 pub mod io;
 
+pub mod context;
+mod interrupt;
+
 use core::panic::PanicInfo;
 
-global_asm!(include_str!("arch/riscv32/boot/entry.asm"));
+global_asm!(include_str!("boot/entry.asm"));
+
+#[cfg(feature = "m_mode")]
+global_asm!("
+    .equ xstatus,   0x300
+    .equ xscratch,  0x340
+    .equ xepc,      0x341
+    .equ xcause,    0x342
+    .equ xtval,     0x343
+    .macro XRET\n mret\n .endm
+    .macro TEST_BACK_TO_KERNEL  // s0 == back to kernel?
+        li   s3, 3 << 11
+        and  s0, s1, s3         // mstatus.MPP = 3
+    .endm
+");
+#[cfg(not(feature = "m_mode"))]
+global_asm!("
+    .equ xstatus,   0x100
+    .equ xscratch,  0x140
+    .equ xepc,      0x141
+    .equ xcause,    0x142
+    .equ xtval,     0x143
+    .macro XRET\n sret\n .endm
+    .macro TEST_BACK_TO_KERNEL
+        andi s0, s1, 1 << 8     // sstatus.SPP = 1
+    .endm
+");
+global_asm!(r"
+    .equ XLENB,     4
+    .equ XLENb,     32
+    .macro LOAD a1, a2
+        lw \a1, \a2*XLENB(sp)
+    .endm
+    .macro STORE a1, a2
+        sw \a1, \a2*XLENB(sp)
+    .endm
+");
+global_asm!(include_str!("trap/trap.asm"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -20,6 +61,10 @@ pub extern "C" fn rust_main() -> ! {
     let a = "Hello";
     let b = "World";
     println!("{}, {}!", a, b);
+    interrupt::init();
+    unsafe{
+        asm!("ebreak\n"::::);
+    }
     panic!("End of rust_main");
 }
 
