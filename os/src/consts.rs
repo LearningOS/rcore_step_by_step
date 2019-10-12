c3e338916cc83b894e4546088cd7b82c745aa421
fengyuan <511568816@qq.com>
2019-10-12 23:14:07 +0800

    alloc-memory

---
+++ os/src/consts.rs
@@ -0,0 +1,4 @@
+pub const KERNEL_HEAP_SIZE: usize = 0x00a0_0000;
+pub const MEMORY_OFFSET: usize = 0x8000_0000;
+pub const MEMORY_END: usize = 0x8800_0000;
+pub const PAGE_SIZE: usize = 4096;
--- os/src/init.rs
+++ os/src/init.rs
@@ -1,8 +1,9 @@
 global_asm!(include_str!("boot/entry.asm"));
 
 #[no_mangle]
-pub fn rust_main() -> ! {
+pub fn rust_main(hartid: usize, dtb: usize) -> ! {
     crate::interrupt::init();
     crate::clock::init();
+    crate::memory::init();
     loop {}
 }
